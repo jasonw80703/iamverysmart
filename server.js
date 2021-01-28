46162424f42b8e3ec6688e38fa678bcb0229c5f2
Jason Wang <jasonw80702@gmail.com>
2021-01-28 13:38:37 -0800

    update comment for server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./app/models");
 db.sequelize.sync();
 
-// Dev
+// Reset
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
 // });
